09ac53dd23e2fb044a43be08f62c684ba11fc269
zeeusn <zhienfh@gmail.com>
2026-03-01 23:22:10 +1300

    Add probability_lower_bound function in a.py
    Implement a function to calculate the probability lower bound based on test outcomes and deviation.

---
+++ a.py
@@ -0,0 +1,11 @@
+import math
+
+def probability_lower_bound(test_outcomes, deviation):
+    d = len(test_outcomes)
+
+    print(d)
+    return 1-2*math.exp(-2*(deviation**2)*d)
+
+
+
+print(probability_lower_bound([True, False] * 500, 0.05))
